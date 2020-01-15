17883b994ae95bdf00bd60260ea8b4f52d0cd4b3
Lftho <lft.thomaz@gmail.com>
2020-01-15 16:56:58 -0300

    curso ES6+ da rocketseat, completo

---
+++ README.md
@@ -0,0 +1,5 @@
+## Consumino API do Github com estudo focado no ES6+
+
+- Para fazer o buscar no input é preciso procurar por nome de  Organização:
+
+  Exemplo: react-community/react-navigation.
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import api from './api';
+
 class App {
   constructor() {
     //guardar toda lista do repositorios
@@ -5,6 +7,12 @@ class App {
 
     //buscar referencia pelo js
     this.formEl = document.getElementById('repo-form');
+
+    //referencia o input do html
+    this.inputEl = document.querySelector('input[name=repository]');
+
+    //renderizar em tela a nossa lista
+    this.listEl = document.getElementById('repo-list');
 
     this.registerHandlers();
   }
@@ -15,18 +23,83 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
-  addRepository(event) {
+  // uma função de carregar
+  setLoading(loading = true) {
+    if ( loading === true ){
+      let loadingEl = document.createElement('span');
+      loadingEl.appendChild(document.createTextNode('Carregando'));
+      loadingEl.setAttribute('id', 'loading');
+
+      this.formEl.appendChild(loadingEl);
+    } else {
+      document.getElementById('loading').remove();
+    }
+  }
+
+  async addRepository(event) {
     event.preventDefault();
 
-    //adcionando as informaçoes esstatico
+    //pegando o valor do input
+    const repoInput = this.inputEl.value;
+    // verificar se tem alguma coisa escrito dentro do nosso input
+    if ( repoInput.length === 0 ) 
+      return;
+
+    this.setLoading();
+    
+    // Try e o catch para dá um posição se o repositorio existe ou não
+    try { 
+    //se realmente entrou informação dentro input, então buscaremos valores dentro da api
+    const response = await api.get(`/repos/${repoInput}`);
+
+    //destruturação
+    const { name, description, html_url, owner: { avatar_url } } = response.data;
+
+    //adcionando as informaçoes estatico e template literals
     this.repositories.push({
-      name: 'rocketseat.com.br',
-      description: 'Tire a sua ideia do papel e de vida à sua startup',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-      html_url: 'http://github.com/rocketseat/rocketseat.com.br'
+      name,
+      description,
+      avatar_url,
+      html_url,
     });
 
-    console.log(this.repositories);
+    this.inputEl.value = '';
+
+    this.render();
+   } catch(err) {
+     alert('O repositorio não existe');
+   }
+
+   this.setLoading(false);
+  }
+    // Rendenrizar tudo do zero
+    render() {
+      this.listEl.innerHTML = '';
+      //Usado o metodo forEach para renderizar todos os repositorio, pois é o metodo que mais prático do que o map.
+      //Que percorre sem mudar o array do repositories.
+      this.repositories.forEach(repo => {
+        let imgEl = document.createElement('img');
+        imgEl.setAttribute('src', repo.avatar_url);
+
+        let titleEl = document.createElement('strong');
+        titleEl.appendChild(document.createTextNode(repo.name));
+
+        let descriptionEl = document.createElement('p');
+        descriptionEl.appendChild(document.createTextNode(repo.description));
+
+        let linkEl = document.createElement('a');
+        linkEl.setAttribute('target', '_blank' );
+        linkEl.setAttribute('href', repo.html_url);
+        linkEl.appendChild(document.createTextNode('Acessar'));
+
+        let listItemEl = document.createElement('li');
+        listItemEl.appendChild(imgEl);
+        listItemEl.appendChild(titleEl);
+        listItemEl.appendChild(descriptionEl);
+        listItemEl.appendChild(linkEl);
+
+        this.listEl.appendChild(listItemEl);
+    });
   }
 }
 
